3dc576c5e6589283ba18e37c7eedda35846a5419
Samir Merchant <fanged.parakeet@gmail.com>
2018-05-30 03:31:55 -0500

    begin mailer

--- task.php
+++ task.php
@@ -113,10 +113,27 @@
 
 					$logger->message($job["jobId"], "Sending email...");
 
-					// $dispatcher->updateJob($job["jobId"], array(
-					// 	"statusCode" => 0,
-					// 	"status" => "Finished"
-					// ));
+					$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+
+				    //Server settings
+				    $mail->isSMTP();                                      // Set mailer to use SMTP
+				    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+				    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+				    $mail->Username = 'noreply@visaexperiences.inition.co.uk';                 // SMTP username
+				    $mail->Password = 'OnAFreeFromPSG';                           // SMTP password
+				    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+				    $mail->Port = 587;                                    // TCP port to connect to
+
+				    //Recipients
+				    $mail->setFrom('noreply@visaexperiences.inition.co.uk', 'Zlatan');
+				    $mail->addAddress($job["email"], $job["name"]);     // Add a recipient
+
+				    //Content
+				    $mail->isHTML(true);                                  // Set email format to HTML
+				    $mail->Subject = 'Shooting for the Stars Video';
+				    $mail->Body    = '<a href="' . $job["finalLink"] . '" target="_blank">CLICK TO DOWNLOAD VIDEO!</a>';
+
+				    $mail->send();
 					break;
 
 			}
